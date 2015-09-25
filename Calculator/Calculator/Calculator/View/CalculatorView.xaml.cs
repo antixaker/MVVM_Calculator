cdc73976c030a15f2380b053294ef336994c1bc9
antixaker <proskuraaleksandr93@gmail.com>
2015-09-25 17:07:36 +0300

    Code cleaning in CalculatorView

--- Calculator/Calculator/Calculator/View/CalculatorView.xaml.cs
+++ Calculator/Calculator/Calculator/View/CalculatorView.xaml.cs
@@ -5,53 +5,11 @@ namespace Calculator
 {
     public partial class CalculatorView : ContentPage
     {
-        //IInputManager displayManager;
-        //ICaclProcessor mathProcessor;
 
         public CalculatorView()
         {
             InitializeComponent();
-
-            //displayManager = new InputManager();
-            //mathProcessor = new CaclProcessor();
         }
 
-        //void OnButtonClicked(object sender, EventArgs e)
-        //{
-        //    Button tmp = sender as Button;
-        //    if (tmp == null)
-        //        return;
-
-        //    Display.Text = displayManager.AddSymbol(Display.Text, tmp.Text);
-        //}
-
-        //void OnResetClicked(object sender, EventArgs e)
-        //{
-        //    Button tmp = sender as Button;
-        //    if (tmp == null)
-        //        return;
-
-        //    Display.Text = displayManager.ClearAll();
-        //}
-
-        //void OnResultClicked(object sender, EventArgs e)
-        //{
-        //    Button tmp = sender as Button;
-        //    if (tmp == null)
-        //        return;
-
-        //    var res = mathProcessor.Calculate(Display.Text);
-        //    if (res != null)
-        //        Display.Text = res.ToString();
-        //}
-
-        //void OnRemoveLastSymbolClicked(object sender, EventArgs e)
-        //{
-        //    Button tmp = sender as Button;
-        //    if (tmp == null)
-        //        return;
-
-        //    Display.Text = displayManager.RemoveLastSymbol(Display.Text);
-        //}
     }
 }
